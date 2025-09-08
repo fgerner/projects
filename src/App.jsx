0440323de347c2070bfc5f02eb2ad93f8e1ef456
fgerner <fred.gerner@icloud.com>
2025-09-08 17:28:29 +1000

    feat: enable project addition functionality in App and NewProject components
    - Add `handleAddProject` function in App component to manage new project creation
    - Pass `onAdd` prop to NewProject for saving new project data
    - Enhance NewProject component with input refs and `handleSave` logic for capturing user input

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,23 @@ function App() {
         )
     }
 
+    function handleAddProject(projectData) {
+        setProjectsState(prevState => {
+            const newProject = {
+                id: crypto.randomUUID(),
+                ...projectData,
+            }
+            return {
+                ...prevState,
+                projects: [...prevState.projects, newProject]
+            }
+        })
+    }
+
+    console.log(projectsState)
+
     if (projectsState.selectedProjectId === null) {
-        content = <NewProject/>
+        content = <NewProject onAdd={handleAddProject}/>
     } else if (projectsState.selectedProjectId === undefined) {
         content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
     }
